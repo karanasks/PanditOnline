96cf64b46bf71721c765d4e39d8e66a52c5327a5
Karan Fatnani <karan.fatnani@aquariansoft.in>
2018-11-19 02:53:48 +0530

    Added Blank Repository Files/Classes for all the database Tables

---
+++ EPandit.Repository/Repositories/PanditDetailRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EPandit.Repository.Repositories
+{
+    public class PanditDetailRepository
+    {
+    }
+}
